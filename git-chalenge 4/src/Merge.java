a4050b376f501575434a51b7b497dbb14b4e1a93
Vaishnavi-D-R <vaishnavidr200@gmail.com>
2021-02-05 09:42:27 +0530

    replaced addition with subtraction

--- git-chalenge 4/src/Merge.java
+++ git-chalenge 4/src/Merge.java
@@ -3,9 +3,9 @@ public class Merge {
 	public static void main(String []args) {
 		System.out.println("Hello");
 		
-		int data1=10;
-		int data2=19;
-		System.out.println(data1*data2);
+		int x=10;
+		int y=19;
+		System.out.println(x-y);
 
 	}
 }
