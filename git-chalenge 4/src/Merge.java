d2c589f972a5129181fd43ee2871a827bdd07ceb
Vaishnavi-D-R <78353427+Vaishnavi-D-R@users.noreply.github.com>
2021-02-05 09:41:31 +0530

    replaced addition with multiplication

--- git-chalenge 4/src/Merge.java
+++ git-chalenge 4/src/Merge.java
@@ -3,9 +3,9 @@ public class Merge {
 	public static void main(String []args) {
 		System.out.println("Hello");
 		
-		int data1=10;
-		int data2=19;
-		System.out.println(data1*data2);
+		int a=10;
+		int b=19;
+		System.out.println(a*b);
 
 	}
 }
